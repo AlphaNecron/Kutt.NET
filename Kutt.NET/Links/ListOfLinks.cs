cc515d88ed78ed33f45b4700fc71fb6657d59c23
v11 <nguyenvanthanhlam@venus-11.xyz>
2021-06-22 14:34:51 +0700

    Added Summaries
    Added Summaries for ListOfLinks.cs

--- Kutt.NET/Links/ListOfLinks.cs
+++ Kutt.NET/Links/ListOfLinks.cs
@@ -2,17 +2,32 @@
 
 namespace Kutt.NET.Links
 {
+    /// <summary>
+    /// Json Propeties for ListOfLinks
+    /// </summary>
     public class ListOfLinks
     {
+        /// <summary>
+        /// The limit
+        /// </summary>
         [JsonProperty("limit")]
         public int Limit { get; private set; }
-
+        
+        /// <summary>
+        /// Skips
+        /// </summary>
         [JsonProperty("skip")]
         public int Skip { get; private set; }
-
+        
+        /// <summary>
+        /// Total Amount
+        /// </summary>
         [JsonProperty("total")]
         public int Total { get; private set; }
-
+        
+        /// <summary>
+        /// Datas which content in Link.cs
+        /// </summary>
         [JsonProperty("data")]
         public Link[] Links { get; private set; }
     }
