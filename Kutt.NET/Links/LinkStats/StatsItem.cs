904f35918ea9401e32e93ca064fb5c3c1a2a6914
v11 <nguyenvanthanhlam@venus-11.xyz>
2021-06-22 14:42:46 +0700

    Added Summaries
    Added Summaries for StatsItem.cs

--- Kutt.NET/Links/LinkStats/StatsItem.cs
+++ Kutt.NET/Links/LinkStats/StatsItem.cs
@@ -2,11 +2,20 @@
 
 namespace Kutt.NET.Links.LinkStats
 {
+    /// <summary>
+    /// Json Properties for StatsItem
+    /// </summary>
     public class StatsItem
     {
+        /// <summary>
+        /// Stats
+        /// </summary>
         [JsonProperty("stats")]
         public Stats Stats { get; private set; }
-
+        
+        /// <summary>
+        /// Viewers
+        /// </summary>
         [JsonProperty("views")]
         public int[] Views { get; private set; }
     }
