d6c8a4399404386d4f6db2aae797d8b587f3aefe
v11 <nguyenvanthanhlam@venus-11.xyz>
2021-06-22 14:32:22 +0700

    Added Summaries
    Summaries for Domain.cs

--- Kutt.NET/Domains/Domain.cs
+++ Kutt.NET/Domains/Domain.cs
@@ -3,23 +3,44 @@ using Newtonsoft.Json;
 
 namespace Kutt.NET.Domains
 {
+    /// <summary>
+    /// Json Properties for Domain
+    /// </summary>
     public class Domain
     {
+        /// <summary>
+        /// Domain Address
+        /// </summary>
         [JsonProperty("address")]
         public string Address { get; private set; }
-
+        
+        /// <summary>
+        /// Domain Banned
+        /// </summary>
         [JsonProperty("banned")]
         public bool IsBanned { get; private set; }
-
+        
+        /// <summary>
+        /// ID
+        /// </summary>
         [JsonProperty("id")]
         public string Uuid { get; private set; }
-
+        
+        /// <summary>
+        /// Created At
+        /// </summary>
         [JsonProperty("created_at")]
         public DateTime CreatedAt { get; private set; }
-
+        
+        /// <summary>
+        /// Link Homepage
+        /// </summary>
         [JsonProperty("homepage")]
         public string Homepage { get; private set; }
-
+        
+        /// <summary>
+        /// Updated At
+        /// </summary>
         [JsonProperty("updated_at")]
         public DateTime UpdatedAt { get; private set; }
     }
