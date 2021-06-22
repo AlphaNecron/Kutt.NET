31c177124915153dd329989f0b0155c9c93876de
v11 <nguyenvanthanhlam@venus-11.xyz>
2021-06-22 14:36:10 +0700

    Added Summaries
    Added Summaries for Data.cs

--- Kutt.NET/Links/LinkStats/Data.cs
+++ Kutt.NET/Links/LinkStats/Data.cs
@@ -2,11 +2,20 @@
 
 namespace Kutt.NET.Links.LinkStats
 {
+    /// <summary>
+    /// Json Properties for Data.cs
+    /// </summary>
     public class Data
     {
+        /// <summary>
+        /// Name
+        /// </summary>
         [JsonProperty("name")]
         public string Name { get; private set; }
-
+        
+        /// <summary>
+        /// Value
+        /// </summary>
         [JsonProperty("value")]
         public int Value { get; private set; }
     }
