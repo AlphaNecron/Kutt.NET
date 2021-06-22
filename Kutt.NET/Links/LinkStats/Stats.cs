e7adaccd0c404c8d7372a5723e4a65a25b232464
v11 <nguyenvanthanhlam@venus-11.xyz>
2021-06-22 14:41:34 +0700

    Added Summaries
    Added Summaries for Stats.cs

--- Kutt.NET/Links/LinkStats/Stats.cs
+++ Kutt.NET/Links/LinkStats/Stats.cs
@@ -2,17 +2,32 @@
 
 namespace Kutt.NET.Links.LinkStats
 {
+    /// <summary>
+    /// Json Properties for Domain Stats
+    /// </summary>
     public class Stats
     {
+        /// <summary>
+        /// Browser
+        /// </summary>
         [JsonProperty("browser")]
         public Data[] BrowserStats { get; private set; }
-
+        
+        /// <summary>
+        /// Operating System
+        /// </summary>
         [JsonProperty("os")]
         public Data[] OsStats { get; private set; }
-
+        
+        /// <summary>
+        /// Country
+        /// </summary>
         [JsonProperty("country")]
         public Data[] CountryStats { get; private set; }
-
+        
+        /// <summary>
+        /// Referrer Stats
+        /// </summary>
         [JsonProperty("referrer")]
         public Data[] ReferrerStats { get; private set; }
     }
