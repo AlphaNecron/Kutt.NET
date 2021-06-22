eb85bef7a3c355642c7200ecb8b6dc407e432e70
Nguyen Thanh Quang <admin@xwork.space>
2021-06-22 14:52:05 +0700

    Update Link.cs

--- Kutt.NET/Links/Link.cs
+++ Kutt.NET/Links/Link.cs
@@ -9,37 +9,37 @@ namespace Kutt.NET.Links
     public class Link
     {
         /// <summary>
-        /// Domain Address
+        /// Link slug, eg: abc123
         /// </summary>
         [JsonProperty("address")]
         public string Address { get; private set; }
         
         /// <summary>
-        /// Domain Banned
+        /// Link status
         /// </summary>
         [JsonProperty("banned")]
         public bool IsBanned { get; private set; }
 
         /// <summary>
-        /// Domain CreatedAt
+        /// Creation date
         /// </summary>
         [JsonProperty("created_at")]
         public DateTime CreatedAt { get; private set; }
         
         /// <summary>
-        /// Id
+        /// Link unique ID
         /// </summary>
         [JsonProperty("id")]
         public string Uuid { get; private set; }
         
         /// <summary>
-        /// Link
+        /// Shortened link
         /// </summary>
         [JsonProperty("link")]
         public string ShortUrl { get; private set; }
         
         /// <summary>
-        /// Target Domain
+        /// Original link
         /// </summary>
         [JsonProperty("target")]
         public string LongUrl { get; private set; }
@@ -51,13 +51,13 @@ namespace Kutt.NET.Links
         public string Description { get; private set; }
         
         /// <summary>
-        /// Updated at
+        /// Update date
         /// </summary>
         [JsonProperty("updated_at")]
         public DateTime UpdatedAt { get; private set; }
         
         /// <summary>
-        /// Visit Count
+        /// Visit count
         /// </summary>
         [JsonProperty("visit_count")]
         public int Clicks { get; private set; }
